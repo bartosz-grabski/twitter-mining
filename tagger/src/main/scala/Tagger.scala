1667828ffe44211f39f28bb0c85f1adf5a01c6f8
Bartosz Grabski <b.grabski92@gmail.com>
2014-06-01 22:51:32 +0200

    Vectorizer work in progress

---
+++ tagger/src/main/scala/Tagger.scala
@@ -0,0 +1,7 @@
+object Tagger extends App {
+	
+	val vectorizer = new Vectorizer()
+	vectorizer.createVectorForContent(Array("asd","bsd"),1)
+
+
+}
--- tagger/src/main/scala/Vectorizer.scala
+++ tagger/src/main/scala/Vectorizer.scala
@@ -1,3 +1,19 @@
 class Vectorizer {
 	
+	private var words = Map("asd" -> 2,"bsd" -> 1,"csd" -> 0)
+
+	//Creates vector. Last element of created vector is the label value
+	//(tag,classification)
+	
+	def createVectorForContent(content : Array[String], label : Int) = {
+		val vector = new Array[Int](content.length + 1)
+		content.foreach { t =>
+			if (words.contains(t)) {
+				vector(words(t)) = 1	
+			}
+		}
+		vector(vector.length-1) = label;
+		vector
+	}
+
 }
